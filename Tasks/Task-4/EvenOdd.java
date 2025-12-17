6148dbac766f7743f3434132f84be064d73ffdf0
Kilaripadmavathikumari <kpadmavathikumari@gamil.com>
2025-12-17 19:07:17 +0530

    Streams

--- Tasks/Task-4/EvenOdd.java
+++ Tasks/Task-4/EvenOdd.java
@@ -7,14 +7,14 @@ public class EvenOdd
     {
        List<Integer> list=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
        System.err.println("using Partiton");
-       Map<Boolean,Long> ml=list.stream()
+       Map<Boolean,Long> ml=list.stream()                // partition return Boolean and count
        .sorted()
        .collect(Collectors.partitioningBy( n->n%2==0 , Collectors.counting()));
        System.err.println(ml);
  
      List<Integer> list1=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
      System.err.println("Using Grouping");
-       Map<Boolean,List<Integer>> m=list1.stream()
+       Map<Boolean,List<Integer>> m=list1.stream()             // Groupby Boolean and list
        .sorted()
        .collect(Collectors.groupingBy(n->n%2==0));
        System.err.println(m);
